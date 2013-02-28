6d8d21acea35ce838bfaba517f1548a46601cbd1
Romain Hardy <romain.hardy17@gmail.com>
2013-02-28 12:14:42 -0600

    Add session service, doctrine service and twig service

--- src/app.php
+++ src/app.php
@@ -8,8 +8,28 @@ use Igorw\Silex\ConfigServiceProvider;
 $app = new Silex\Application();
 
 // Add config service
-$env = getenv('APP_ENV') ?: 'prod';
+$env = getenv('APP_ENV') ?: 'prod';	
 $app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/$env.json"));
+
+// Add session Service
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+// Add Doctrine Service
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(        
+            'driver'    => $app['config_db']['driver'],
+            'host'      => $app['config_db']['host'],
+            'dbname'    => $app['config_db']['dbname'],
+            'user'      => $app['config_db']['user'],
+            'password'  => $app['config_db']['password'],
+            'charset'   => $app['config_db']['charset'],
+        ),
+    ));
+// Add Twig service
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/../vendor/Twig/lib',
+));
 
 
 // ******* Application *****
