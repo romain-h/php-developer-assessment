70917a7d5cdee2e5c1a9b52b11f726736ecfb731
Romain Hardy <romain.hardy17@gmail.com>
2013-02-28 11:54:29 -0600

    add Igorw config Service provider

--- src/app.php
+++ src/app.php
@@ -1,11 +1,18 @@
 <?php
-// Bootstrap
+// (*****  Bootstrap *****
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Application;
+use Igorw\Silex\ConfigServiceProvider;
 
 $app = new Silex\Application();
 
+// Add config service
+$env = getenv('APP_ENV') ?: 'prod';
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/$env.json"));
+
+
+// ******* Application *****
 $app->get('/', function(Application $app) {
 	return "Init ok";
 });
